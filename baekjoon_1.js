cd317ce7e60cb9259cd64167f2f9a10e53ad9b36
ovozzx <takiwow19@gmail.com>
2025-06-27 01:13:57 +0900

    250627 map study

--- baekjoon_1.js
+++ baekjoon_1.js
@@ -19,3 +19,19 @@ console.log("출력 테스트");
 
 // #1 Hello World
 console.log("Hello World!");
+
+// #2 A+B
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split(' ');
+
+const a = Number(input[0]);
+const b = Number(input[1]);
+
+console.log(a + b);
+/* ✅ 설명
+readFileSync('/dev/stdin'): 백준에서는 표준 입력을 통해 값을 받습니다.
+
+.toString().trim().split(' '): 입력을 문자열로 만든 후, 공백을 기준으로 나눕니다.
+
+Number(): 문자열을 숫자로 변환합니다.
+
+console.log(): 결과를 출력합니다.*/
