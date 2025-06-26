14fb5f4cfd84eb63228ec9e2c2d620796147ed7a
ovozzx <takiwow19@gmail.com>
2025-06-27 01:20:05 +0900

    250627 map study

--- test.js
+++
@@ -1,4 +0,0 @@
-var heading = document.querySelector('#heading'); //id="heading"인 요소를 찾아서 heading 변수에 저장 
-    heading.onclick = function() { //클릭했을 때, 실행할 함수 정의
-        heading.style.color = "red";
-    }
